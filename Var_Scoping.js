0ab93d1a957d6c359c8d7745d3799f2f64229dbd
Burnee <76781080+burn-e-e@users.noreply.github.com>
2024-08-07 20:05:31 +0800

    Push

--- Var_Scoping.js
+++ Var_Scoping.js
@@ -1,9 +1,9 @@
-function scoping(){
+function var_scoping(){
     if(true){
         var varVariable = "I'm a var variable.";
-        let letVariable = "I'm a let variable.";
+      //let letVariable = "I'm a let variable."; //! ReferenceError: letVariable is not defined
     }
     console.log(varVariable);
     console.log(letVariable);
 }
-scoping();
+var_scoping();
